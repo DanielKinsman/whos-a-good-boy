257664369f944a09ded3cea404f8440ad0c81ba7
Daniel Kinsman <danielkinsman@riseup.net>
2024-07-10 08:49:17 +1000

    height override

--- gdproject/wrist_ui.gd
+++ gdproject/wrist_ui.gd
@@ -1,9 +1,30 @@
-extends CenterContainer
+extends MarginContainer
+
+@onready var height_button: CheckButton = $GContainer/HBoxContainer/CheckButton
+@onready var height_label: Label = $GContainer/HBoxContainer/HeightLabel
+@onready var height_slider: Slider  = $GContainer/HeightSlider
 
 
 func _ready() -> void:
-    $HBoxContainer/QuitButton.connect("button_up", self.quit)
+    $GContainer/QuitButton.connect("button_up", self.quit)
+    height_slider.value_changed.connect(height_changed)
+    height_button.toggled.connect(override_height_toggled)
 
 
 func quit() -> void:
     get_tree().quit()
+
+
+func height_changed(value: float) -> void:
+    height_label.text = "Override player height: %0.2f m" % value
+    var body: XRToolsPlayerBody = XRHelpers.get_xr_origin(self).get_node("PlayerBody")
+    body.override_player_height("doggo", value)
+
+
+func override_height_toggled(on: bool) -> void:
+    height_slider.editable = on
+    if on:
+        height_changed(height_slider.value)
+    else:
+        var body: XRToolsPlayerBody = XRHelpers.get_xr_origin(self).get_node("PlayerBody")
+        body.override_player_height("doggo", -1.0)
