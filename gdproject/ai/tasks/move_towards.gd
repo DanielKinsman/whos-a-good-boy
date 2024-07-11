301267fb8cab8236df79d3b68fa15d60a9c23f26
Daniel Kinsman <danielkinsman@riseup.net>
2024-07-11 14:51:39 +1000

    tweak jump params

--- gdproject/ai/tasks/move_towards.gd
+++ gdproject/ai/tasks/move_towards.gd
@@ -32,14 +32,14 @@ func _tick(delta: float) -> Status:
     # TODO proper air intercept calculations based on object's trajectory and our trajectory
     if (vector_to_target.max_axis_index() == Vector3.AXIS_Y and
             vector_to_target.y > 2.0 and vector_to_target.y < 4.0 and
-            ground_vector_to_target.length() < 2.0):
+            ground_vector_to_target.length() < 1.0):
         #dog.velocity.y = 3.5
         # dog jumps about 1/3 of a second in to the animation
         if blackboard.get_var(&"jump_requested"):
             return RUNNING
 
         blackboard.set_var(&"jump_requested", true)
-        dog.get_tree().create_timer(0.33).timeout.connect(func() -> void: dog.velocity.y = 5.0)
+        dog.get_tree().create_timer(0.33).timeout.connect(func() -> void: dog.velocity.y = 4.5)
         dog.animation.set("parameters/OneShotBark/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
         if not dog.animation.get("parameters/OneShotJump/active"):
             dog.animation.set("parameters/OneShotJump/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
