0335549d1ae96a7e336161455750d4c9abb79d0d
Daniel Kinsman <danielkinsman@riseup.net>
2024-07-12 17:22:21 +1000

    tweak dog jump values

--- gdproject/ai/tasks/move_towards.gd
+++ gdproject/ai/tasks/move_towards.gd
@@ -17,7 +17,7 @@ func _tick(delta: float) -> Status:
     var vector_to_target := dog.move_target.global_position - dog.global_position
     if not dog.is_on_floor():
         blackboard.set_var(&"jump_requested", false)
-        if vector_to_target.length() <= tolerance * 2.0:  # cheat in mid air
+        if vector_to_target.length() <= tolerance * 3.0:  # cheat in mid air
             return SUCCESS
         else:
             return RUNNING
@@ -30,14 +30,14 @@ func _tick(delta: float) -> Status:
     # if direction to target is more up than across, and within ?m and ?m high and within ?m accross, jump
     # TODO proper air intercept calculations based on object's trajectory and our trajectory
     if (vector_to_target.max_axis_index() == Vector3.AXIS_Y and
-            vector_to_target.y > 2.0 and vector_to_target.y < 4.0 and
+            vector_to_target.y > 3.0 and vector_to_target.y < 4.5 and
             ground_vector_to_target.length() < 1.0):
         # dog jumps about 1/3 of a second in to the animation
         if blackboard.get_var(&"jump_requested"):
             return RUNNING
 
         blackboard.set_var(&"jump_requested", true)
-        dog.get_tree().create_timer(0.33).timeout.connect(func() -> void: dog.velocity.y = 4.5)
+        dog.get_tree().create_timer(0.33).timeout.connect(jump)
         dog.animation.set("parameters/OneShotBark/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
         if not dog.animation.get("parameters/OneShotJump/active"):
             dog.animation.set("parameters/OneShotJump/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
@@ -45,3 +45,9 @@ func _tick(delta: float) -> Status:
         dog.velocity += ground_vector_to_target.normalized() * acceleration * delta
 
     return RUNNING
+
+
+func jump() -> void:
+    dog.velocity.y = 4.5
+    dog.velocity.x *= 1.25
+    dog.velocity.z *= 1.25
